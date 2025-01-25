c2247c66d93504979922eeecd6888a53e87df9cb
Vivesh kumar <vivesh103@gmail.com>
2025-01-25 22:21:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let result =''
+	let flag =0;
+	
+	
+	for(let i=0;i<str.length;i++){
+		let count =0;
+		let first_occ =str.charAt(i);
+		for(let j =0;j<str.length;j++){
+			if(str.charAt(j)==first_occ){
+				count +=1;
+			}
+		}
+		if(count ==1){
+         result+=first_occ;
+		 flag =1;
+		}
+	}
+	if(flag !=0){
+		return result;
+	}
+	else{
+		return null;
+	}
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
